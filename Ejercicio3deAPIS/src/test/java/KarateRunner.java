031103228de5a3905f81921837ece4b2511bd92b
Rodrigo Teran <ttbr101081@gmail.com>
2024-02-26 20:05:27 -0500

    ejercicios

--- Ejercicio3deAPIS/src/test/java/KarateRunner.java
+++
@@ -1,7 +0,0 @@
-import com.intuit.karate.junit5.Karate;
-public class KarateRunner {
-    @Karate.Test
-    Karate testSample() {
-        return Karate.run("classpath:karateTest.feature");
-    }
-}
